d4d43a04991e8a7ecf3706fb4dc6574891789c9a
Yuxuan Zhuang <yuxuan.zhuang@dbb.su.se>
2024-09-02 15:13:54 +0200

    set session

--- ggmolvis/__init__.py
+++ ggmolvis/__init__.py
@@ -6,18 +6,28 @@ Molecular visualization with Blender
 # Add imports here
 from importlib.metadata import version
 import bpy
-from bpy.app.handlers import frame_change_post, load_post, save_post
+from bpy.app.handlers import frame_change_post
 from bpy.app.handlers import persistent
-
 import molecularnodes as mn
 
 __version__ = version("ggmolvis")
 
 # sync to the molecularnodes session
-SESSION = mn.session.get_session()
+try:
+    SESSION = mn.session.get_session()
+# if the module is started without Blender running
+except AttributeError:
+    from molecularnodes import register
+    register()
+    SESSION = mn.session.get_session()
 
 @persistent
 def update_frame(scene):
     for artist in SESSION._ggmolvis:
-        artist.update_frame(scene.frame_current)
+        try:
+            artist.update_frame(scene.frame_current)
+        except:
+            # if the object under the artist is deleted,
+            # remove the artist
+            SESSION._ggmolvis.remove(artist)
 frame_change_post.append(update_frame)
