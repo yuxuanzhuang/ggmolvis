8b434fc604b0747256d7a157e99bf0ead6c88a50
Tyler Reddy <tyler.je.reddy@gmail.com>
2025-03-04 17:48:37 -0700

    WIP, ENH: PR 14 revisions
    * Initial attempt to place text overlay without
    user provided `location` and `rotation`.
    
    [ci skip]

--- ggmolvis/sceneobjects/texts.py
+++ ggmolvis/sceneobjects/texts.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 
 import bpy
+from mathutils import Vector
 from .base import SceneObject
 
 
@@ -42,10 +43,23 @@ class Text(SceneObject):
         )
 
     def _text_creator(self):
-        bpy.ops.object.text_add(location=self.location)
+        bpy.ops.object.text_add()
         text_object = bpy.context.active_object
         text_object.data.size = self.text_size
-        text_object.rotation_euler = self.rotation
+        scene = bpy.context.scene
+        camera = scene.camera
+        cam_location = camera.location
+        text_object.data.extrude = 0.2
+        text_object.location = cam_location + camera.matrix_world @ Vector((0, 0, 10))
+        direction_to_camera = (cam_location - text_object.location).normalized()
+        rot_quat = direction_to_camera.to_track_quat('-Z', 'Y')
+        text_object.rotation_mode = 'QUATERNION'
+        text_object.rotation_quaternion = rot_quat
+        #constraint = text_object.constraints.new(type='TRACK_TO')
+        #constraint.target = camera
+        #constraint.track_axis = 'TRACK_Z'
+        #constraint.up_axis = 'UP_Z'
+        #bpy.context.view_layer.update()
         return text_object
 
     def _create_object(self):
