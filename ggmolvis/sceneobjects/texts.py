579cf2285d40addb63c1234078d3147a95a9efe3
Tyler Reddy <tyler.je.reddy@gmail.com>
2025-03-05 20:57:28 -0700

    WIP, ENH: PR 14 revisions
    * Use constraints more correctly for frame count text placement.
    Still needs work for getting the y coordinate of the text properly
    adjusted as focal length changes.
    
    [ci skip]

--- ggmolvis/sceneobjects/texts.py
+++ ggmolvis/sceneobjects/texts.py
@@ -1,8 +1,8 @@
+import math
 from typing import Optional
 
 
 import bpy
-from mathutils import Vector
 from .base import SceneObject
 
 
@@ -46,20 +46,16 @@ class Text(SceneObject):
         bpy.ops.object.text_add()
         text_object = bpy.context.active_object
         text_object.data.size = self.text_size
-        scene = bpy.context.scene
-        camera = scene.camera
-        cam_location = camera.location
-        text_object.data.extrude = 0.2
-        text_object.location = cam_location + camera.matrix_world @ Vector((0, 0, 10))
-        direction_to_camera = (cam_location - text_object.location).normalized()
-        rot_quat = direction_to_camera.to_track_quat('-Z', 'Y')
-        text_object.rotation_mode = 'QUATERNION'
-        text_object.rotation_quaternion = rot_quat
-        #constraint = text_object.constraints.new(type='TRACK_TO')
-        #constraint.target = camera
-        #constraint.track_axis = 'TRACK_Z'
-        #constraint.up_axis = 'UP_Z'
-        #bpy.context.view_layer.update()
+        #text_object.data.extrude = 0.2
+        # TODO: avoid hard coding camera of interest for the render;
+        # probably by unifying to a single scene camera
+        camera = bpy.data.objects["atoms_camera"]
+        try:
+            con = text_object.constraints["Copy Transforms"]
+        except KeyError:
+            con = text_object.constraints.new("COPY_TRANSFORMS")
+        con.target = camera
+        con.mix_mode = 'BEFORE_FULL'
         return text_object
 
     def _create_object(self):
@@ -71,4 +67,20 @@ class Text(SceneObject):
         return self.text_object
 
     def _update_frame(self, frame):
+        # TODO: with multiple cameras and this Text object
+        # not knowing which camera will be used for rendering,
+        # we want to avoid manually specifying the identity of that
+        # camera (probably by unifying to a single camera for the entire
+        # scene?)
+        camera = bpy.data.objects["atoms_camera"]
+        cam_data = camera.data
+        focal_length = cam_data.lens
+        sensor_width = cam_data.sensor_width
+        fov = 2 * math.tan(math.radians(sensor_width / (2 * focal_length)))
+        adj_factor = 70
+        new_scale = fov * adj_factor
+        self.text_object.scale = (new_scale, new_scale, new_scale)
+        # at larger focal length we need a smaller y drop
+        y_drop = -1.8 + (focal_length / 100)
+        self.text_object.location = (-0.5, y_drop, -5)
         self.text_object.data.body = f"{self.frame_counter}{frame}"
