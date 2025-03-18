28d4ec58dfa61443ce987d0347067b72983bd933
Yuxuan Zhuang <yuxuan.zhuang@dbb.su.se>
2025-03-18 16:12:24 -0700

    add render test

--- ggmolvis/tests/test_molecules.py
+++ ggmolvis/tests/test_molecules.py
@@ -35,8 +35,8 @@ def test_molecule_connected(ggmolvis, atomgroup):
     ggmolvis.molecule(atomgroup)
     # set frame to 1
     bpy.context.scene.frame_set(5)
-    print(ggmolvis.session)
     assert atomgroup.universe.trajectory.frame == 5
+
 
 @mark.parametrize('material', AVAILABLE_MATERIALS)
 def test_show_molecule_with_material(ggmolvis, atomgroup, material):
@@ -51,3 +51,29 @@ def test_show_molecule_with_style(ggmolvis, atomgroup, style):
 
     # Test the function with the given style
     ggmolvis.molecule(atomgroup, style=style)
+
+def test_render_scene(ggmolvis, atomgroup):
+    mol = ggmolvis.molecule(atomgroup)
+    ggmolvis.render()
+
+# add test for rendering
+render_options = [
+    {'frame': 0, },
+    {'frame_range': (0, 4, 1)},
+    {'track': True},
+]
+
+@mark.parametrize('render_options', render_options)
+def test_render(ggmolvis, atomgroup, render_options):
+    mol = ggmolvis.molecule(atomgroup)
+    ggmolvis.render(mol, **render_options,
+                    resolution=(300, 200),
+                    )
+
+def test_render_error_frame_range(ggmolvis, atomgroup):
+    mol = ggmolvis.molecule(atomgroup)
+    with pytest.raises(ValueError):
+        ggmolvis.render(mol, frame=0, frame_range=(0, 4, 1))
+    
+    with pytest.raises(ValueError, match='frame_range must be'):
+        ggmolvis.render(mol, frame_range=(0, 4))
