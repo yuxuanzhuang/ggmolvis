3fc329496564909d7ca731797690bcb1248f964f
Tyler Reddy <tyler.je.reddy@gmail.com>
2025-03-21 14:18:58 -0600

    TST: add render testing, mark as xslow
    * This adds the first test that actually makes assertions about
    the pixel content of `ggmolvis`-rendered images. It turns out
    that regular render calls do "ok" in GitHub actions, but if you
    use the compositor there seem to be issues. Since the test added here
    uses the compositor to set the background color, it is marked `xslow`
    so that the user/developer must opt in to run it.
    
    * This approach has the advantage that we can start marking assertions
    about the expected appearance of renders, but of course it has the
    drawback that we can only run these tests locally for now (or using
    one of the services that offers GPU CI runners, which some larger OSS
    projects do, but usually has a cost). I think it is still better than
    nothing given that we could at least bisect on newly-introduced problems
    locally even if it is unfortunately rather easy for bugs to slip past
    the CI this way. This is a common approach upstream for tests that are
    not CI tractable (i.e., incredibly slow, or require unreasonably large
    amounts of memory to reproduce the original problem).
    
    * For a bit of a dissection on composition causing crashes in GHA CI,
    see recent testing on my fork at:
    https://github.com/tylerjereddy/ggmolvis/pull/1

--- ggmolvis/tests/conftest.py
+++ ggmolvis/tests/conftest.py
@@ -7,9 +7,22 @@ Global pytest fixtures
 # More information at
 # https://docs.pytest.org/en/stable/how-to/fixtures.html#scope-sharing-fixtures-across-classes-modules-packages-or-session
 
+import os
 import pytest
 
 @pytest.fixture
 def ggmv():
     from ggmolvis import GGMolVis
     return GGMolVis()
+
+
+def pytest_runtest_setup(item):
+    mark = item.get_closest_marker("xslow")
+    if mark is not None:
+        try:
+            v = int(os.environ.get('GGMOLVIS_XSLOW', '0'))
+        except ValueError:
+            v = False
+        if not v:
+            pytest.skip("very slow test; "
+                        "set environment variable GGMOLVIS_XSLOW=1 to run it")
--- ggmolvis/tests/test_render.py
+++ ggmolvis/tests/test_render.py
@@ -1,5 +1,8 @@
 from ggmolvis.ggmolvis import GGMolVis
 from ggmolvis.tests.data import PSF, DCD
+from MDAnalysisTests.datafiles import GRO
+from PIL import Image
+import numpy as np
 
 from MDAnalysis import Universe
 from ggmolvis.utils import MOL_AVAILABLE_STYLES, AVAILABLE_MATERIALS
@@ -76,3 +79,30 @@ def test_render_error_frame_range(ggmv, atomgroup, render_mp4):
         ggmv.render(mol, frame_range=(0, 4),
                         resolution=(300, 200),
                         filepath=render_mp4)
+
+
+@cleanup
+@pytest.mark.xslow
+def test_render_basic_bg(tmpdir):
+    # confirm correct propagation of background color
+    # and image size specified
+    ggmv = GGMolVis()
+    u = Universe(GRO)
+    mol = ggmv.molecule(u.atoms)
+    with tmpdir.as_cwd():
+        ggmv.render(object=mol,
+                    resolution=(50, 50),
+                    filepath=f"test_red.png",
+                    mode="image",
+                    # red background
+                    composite_bg_rgba=(0.9, 0, 0, 1.0),
+                    lens=35)
+        with Image.open("test_red.png") as img:
+            img_array = np.array(img)
+            red = img_array[..., 0]
+            green = img_array[..., 1]
+            blue = img_array[..., 2]
+    # red should be the dominant channel
+    assert red.sum() > (green.sum() + blue.sum())
+    # image size should be 50 x 50 x 4 channels
+    assert img_array.size == 10_000
