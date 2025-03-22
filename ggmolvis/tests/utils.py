2ce9e232cd16fd51f261db14909e5690887221d9
Yuxuan Zhuang <yuxuan.zhuang@dbb.su.se>
2025-03-22 01:11:14 -0700

    cleanup func

--- ggmolvis/tests/utils.py
+++ ggmolvis/tests/utils.py
@@ -1,6 +1,6 @@
-import pytest
 import bpy
 from functools import wraps
+from ggmolvis.ggmolvis import GGMolVis
 
 def cleanup(func):
     @wraps(func)
@@ -17,8 +17,17 @@ def _clean_up(ggmv):
     """Remove all objects after a test."""
     for name, entity in ggmv._artists_dict.items():
         for artist in entity:
+            # remove blender object
             try:
                 bpy.data.objects.remove(artist.object)
             except Exception:
                 pass
-        ggmv._artists_dict[name] = []
+            # remove MN object
+            try:
+                ggmv._session.remove(artist.trajectory.uuid)
+            except Exception:
+                pass
+    ggmv._session.prune()
+    ggmv._session._ggmolvis = set()
+    ggmv._initialized = False
+    ggmv = GGMolVis()
