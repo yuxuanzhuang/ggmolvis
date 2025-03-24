f7243839ec2ec02cae70040b4399b603b5e749e1
Tyler Reddy <tyler.je.reddy@gmail.com>
2025-03-24 13:57:02 -0600

    TST: PR 39 revisions
    * Update the testing utility _clean_up() function
    to additionally use `read_homefile()` for a cleaner
    blender state between tests. This is similar to the
    approach that Brady uses upstream.

--- ggmolvis/tests/utils.py
+++ ggmolvis/tests/utils.py
@@ -27,6 +27,7 @@ def _clean_up(ggmv):
                 ggmv._session.remove(artist.trajectory.uuid)
             except Exception:
                 pass
+    bpy.ops.wm.read_homefile(app_template="")
     ggmv._session.prune()
     ggmv._session._ggmolvis = set()
     ggmv._initialized = False
